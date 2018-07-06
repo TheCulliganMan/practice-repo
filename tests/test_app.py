231864694c6d36459653531b576eeee6334b58e0
DataScience <DataScience@chrobinson.com>
2018-07-06 08:45:50 -0500

    remove expose

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,10 @@
 import requests
 import json
 
-
 def test_redis_set():
     """Test to see if all methods work"""
-    url = "http://localhost:93/api/v1/redis/helloworld"
+    url = "http://lin1pr1ap1:93/api/v1/redis/helloworld"
     json.loads(requests.post(url, json={"value": "world"}).text)
     assert json.loads(requests.get(url).text) == "world"
     requests.delete(url).text
-    assert json.loads(requests.get(url).text) == ""
+    assert json.loads(requests.get(url).text) is None
